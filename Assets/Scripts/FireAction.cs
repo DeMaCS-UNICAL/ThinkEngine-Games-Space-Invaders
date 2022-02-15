94329a29fa18821be5d1cf329c481c3f4250d7aa
fpacenza <francescopacenza93@gmail.com>
2022-02-15 18:06:49 +0100

    Updated action classes and thinkengine dll

--- Assets/Scripts/FireAction.cs
+++ Assets/Scripts/FireAction.cs
@@ -7,19 +7,16 @@ public class FireAction : Action
 {
     public override void Do()
     {
-        Debug.Log("FIRE ACTION - DO");
         FindObjectOfType<Player>().Shoot();
     }
 
     public override bool Done()
     {
-        Debug.Log("FIRE ACTION - DONE");
         return true;
     }
 
     public override State Prerequisite()
     {
-        Debug.Log("FIRE ACTION - PREREQUISITE");
         //State must be returned using State.WAIT|State.ABORT|State.READY
         return State.READY;
     }
--- Assets/Scripts/MoveAction.cs
+++ Assets/Scripts/MoveAction.cs
@@ -8,20 +8,17 @@ public class MoveAction : Action
     string move { get; set; }
     public override void Do()
     {
-        Debug.Log("MOVE ACTION - DO");
         // Player myPlayer = FindObjectOfType<Player>();
         FindObjectOfType<Player>().Move(move);
     }
 
     public override bool Done()
     {
-        Debug.Log("MOVE ACTION - DONE");
         return true;
     }
 
     public override State Prerequisite()
     {
-        Debug.Log("MOVE ACTION - PREREQUISITE");
         return State.READY;
     }
 }
